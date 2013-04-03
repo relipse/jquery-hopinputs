8dfcef1ee6a474b7db916ebdf45a80c4fc419315
relipse <relipse@gmail.com>
2013-04-02 20:59:44 -0500

    Create jquery.hopinputs.js

---
+++ jquery.hopinputs.js
@@ -0,0 +1,47 @@
+/**
+ * HopInputs jquery plugin
+ *
+ * hop to next input upon maxlength
+ * Usage $(form.ssn1).hopinputs({hopLength: 3, hopToInput: form.ssn2}); 
+ *  or $(form.ssn1).hopinputs({hopLength: 3);
+ *  or $(form.ssn1).hopinputs();  //maxlength of ssn1 is 3
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+(function ($) {
+    jQuery.fn.hopinputs = function(opts){ 
+       if (!opts){ opts = {} }
+       if (!opts.hopLength){
+           opts.hopLength = 'USE_MAXLENGTH';   
+       }
+       if (!opts.hopToInput){
+          opts.hopToInput = 'NEXT_INPUT';  
+       }
+        
+       $(this).each(function(){ 
+          var hoplen, hopipt;
+          if (opts.hopLength == 'USE_MAXLENGTH'){
+             hoplen = $(this).attr('maxlength');
+          }else{
+             hoplen = opts.hopLength;   
+          }
+          if (!hoplen){
+             console.log('opts.hopLength or maxlength attribute are not defined');
+             return false;
+          }
+          
+          if (opts.hopToInput == 'NEXT_INPUT'){
+             hopipt = $(this).next(':input');
+          }else{
+             hopipt = $(opts.hopToInput);
+          }
+          
+          $(this).keyup(function(){
+             if ($(this).val().length >= hoplen){
+                hopipt.focus();
+                //hopipt.select();
+             }
+          });
+       });
+    }
+})(jQuery);
